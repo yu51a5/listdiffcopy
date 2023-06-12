99d0ed9a3776935090750d2909eb42d9c7604714
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-06-12 20:42:19 +0000

    wordpress part works

--- wordpress_code.php
+++ wordpress_code.php
@@ -1,23 +1,15 @@
-// https://stackoverflow.com/questions/25559913/write-from-wordpress-plugin-to-text-file-with-php
-// put_contents
-function save_post_to_file2($post_ID, $post_contents):
-    $file = ABSPATH . 'wp-content/post_text_backup/' . $post_ID . '.txt'; 
-    $open = fopen( $file, "a" ); 
-    $write = fputs( $open, $post_contents ); 
-    fclose( $open );
-}
-
 # https://wordpress.stackexchange.com/questions/381320/register-visits-of-my-pages-in-wordpresss
 # https://blog.hubspot.com/website/wordpress-htaccess
 # https://stackoverflow.com/questions/2354633/retrieve-wordpress-root-directory-path
-function save_to_file($post_ID, $post_contents) {
-    if ( is_user_logged_in() && WP_Filesystem() ) {
+# ttps://wordpress.stackexchange.com/questions/271997/how-to-run-a-function-when-post-is-edited-or-updated-using-publish-post-action
 
+function save_one_post_to_database_($post_ID__) {
+    if ( is_user_logged_in() && WP_Filesystem() ) {
         global $wp_filesystem;
 
         // Set a path for your folder
         $content_dir   = trailingslashit( dirname(ABSPATH) ) . 'backup';
-        $text_file     = trailingslashit( $content_dir ) .  $post_ID . '.txt'; 
+        $text_file     = trailingslashit( $content_dir ) .  $post_ID__ . '.txt'; 
 
         // Create the text file
         if ( ! $wp_filesystem->is_file( $text_file ) ) {
@@ -25,23 +17,34 @@ function save_to_file($post_ID, $post_contents) {
         }
 
         // Add post contents to the file
-        $usernames = $wp_filesystem->put_contents( $text_file, $post_contents, 0755 );
-
+        $usernames = $wp_filesystem->put_contents( $text_file, get_the_content(null, false, $post_ID__ ), 0755 );
     }
 }
 
-// source: https://wordpress.stackexchange.com/questions/271997/how-to-run-a-function-when-post-is-edited-or-updated-using-publish-post-action
-function save_to_database($post_ID, $post_after, $post_before){
-    //echo 'Post ID:';
-    //var_dump($post_ID);
-    // get_the_content( $post_ID )
-    // https://developer.wordpress.org/reference/functions/get_the_content/
-
-    //echo 'Post Object AFTER update:';
-    save_to_file($post_ID, $post_after);
-
-    //echo 'Post Object BEFORE update:';
-    //var_dump($post_before);
+function save_published_post_to_database($post_ID, $post_after, $post_before) {
+  $current_post_status = get_post_status( $post_ID );
+  if ( 'publish' === get_post_status( $post_ID ) ) {
+    save_one_post_to_database_( $post_ID );
+  }
+  save_all_post_to_database();
 }
 
-add_action( 'post_updated', 'save_to_database', 10, 3 ); //don't forget the last argument to allow all three arguments of the function
+function save_unpublished_post_to_database($post_ID) {
+  $current_post_status = get_post_status( $post_ID );
+  if ( 'publish' !== get_post_status( $post_ID ) ) {
+    save_one_post_to_database_( $post_ID );
+  }
+}
+
+function save_all_post_to_database() {
+  $post_IDs = get_posts ( array ( 
+      'fields'          => 'ids', // Only get post IDs
+      'posts_per_page'  => -1
+  ) );
+  foreach ( $post_IDs as $p_ID) {
+      save_one_post_to_database_( $p_ID );
+  }
+}
+
+add_action( 'post_updated', 'save_published_post_to_database', 10, 3 );
+add_action( 'save_post',  'save_unpublished_post_to_database', 10, 1 );
