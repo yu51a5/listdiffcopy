4e0e08700f23910e9edc85372e3428b88ef2807e
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-11-19 18:50:15 +0000

    nice console formatting

--- logging_config.py
+++ logging_config.py
@@ -2,44 +2,39 @@
 # https://docs.python.org/3/howto/logging.html
 # https://stackoverflow.com/questions/7507825/where-is-a-complete-example-of-logging-config-dictconfig
 # https://stackoverflow.com/questions/1343227/can-pythons-logging-format-be-modified-depending-on-the-message-log-level/56944275#56944275
-
-import logging
-#from pythonjsonlogger import jsonlogger
+# https://stackoverflow.com/questions/67241111/python-colored-text-to-the-terminal
+# https://tforgione.fr/posts/ansi-escape-codes/
 
 from datetime import datetime
 import uuid
 import os
+import logging
 
-
-class Colors:
-  grey = "\x1b[0;37m"
-  green = "\x1b[1;32m"
-  yellow = "\x1b[1;33m"
-  red = "\x1b[1;31m"
-  purple = "\x1b[1;35m"
-  blue = "\x1b[1;34m"
-  light_blue = "\x1b[1;36m"
-  reset = "\x1b[0m"
-  blink_red = "\x1b[5m\x1b[1;31m"
-
-class CustomFormatter(logging.Formatter):
+class ConsoleFormatter(logging.Formatter):
   """Logging Formatter to add colors and count warning / errors"""
 
-  bold_red = "\x1b[31;1m"
+  bold_red =    "\x1B[38;2;255;0;0;7m"
+  bold_orange = "\x1B[38;2;255;135;0;7m"
+  bold_yellow = "\x1B[38;2;255;255;0;7m"
+  grey = "\x1b[0;37m"
+  yellow = "\x1b[1;33m"
+  # blink_red = "\x1b[5m\x1b[1;31m"
+  reset = "\x1b[0m"
 
   format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
   FORMATS = {
-    logging.DEBUG: Colors.grey + format + Colors.reset,
-    logging.INFO: Colors.light_blue + '%(message)s' + Colors.reset,
-    logging.WARNING: Colors.yellow + format + Colors.reset,
-    logging.ERROR: Colors.red + format + Colors.reset,
-    logging.CRITICAL: bold_red + format + Colors.reset
+    logging.DEBUG: yellow + format + reset,
+    logging.INFO: grey + '%(message)s' + reset,
+    logging.WARNING: bold_yellow + format + reset,
+    logging.ERROR: bold_orange + format + reset,
+    logging.CRITICAL: bold_red + format + reset
   }
 
   def format(self, record):
-    log_fmt = self.FORMATS.get(record.levelno)
+    log_fmt = self.FORMATS[record.levelno]
     formatter = logging.Formatter(log_fmt)
+    print(dir(record))
     return formatter.format(record)
 
 
@@ -47,22 +42,19 @@ def get_logger(log_name, add_date=True, add_uuid=True, log_dir='logs'):
   logger = logging.getLogger(log_name)
   logger.propagate = False
   logger.setLevel(logging.DEBUG) # the least offensive level that will be taken into account
-
-  filename_ending = ''
-  if add_date:
-    filename_ending += datetime.now().strftime("_%Y_%m_%d_%H_%M_%S")
-  if add_uuid:
-    filename_ending += f'_{uuid.uuid4()}.log'
+  
+  filename_date = datetime.now().strftime("_%Y_%m_%d_%H_%M_%S") if add_date else ''
+  filename_uuid = f'_{uuid.uuid4()}' if add_uuid else ''
 
   # create console handler with a higher log level
   ch = logging.StreamHandler()
   ch.setLevel(logging.DEBUG)
-  ch.setFormatter(CustomFormatter())
+  ch.setFormatter(ConsoleFormatter())
   logger.addHandler(ch)
 
   # create console handler with a higher log level
   for fname, level in (('log', logging.INFO), ('error', logging.WARNING)):
-    filename = log_name + '_' + fname + filename_ending
+    filename = log_name + filename_date + '_' + fname + filename_uuid + '.log'
     if log_dir:
       filename = os.path.join(log_dir, filename)
     ch = logging.FileHandler(filename=filename, mode='a')
