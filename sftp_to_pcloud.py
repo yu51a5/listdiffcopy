80dee6e32208bddcba2b5f2827dedd17fa421e2a
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-12-20 05:42:47 +0000

    added sftp_to_pcloud script

---
+++ sftp_to_pcloud.py
@@ -0,0 +1,25 @@
+import os 
+
+from StorageSFTP import StorageSFTP
+from StorageGitHub import StorageGitHub
+from StoragePCloud import StoragePCloud
+
+from storage_actions import synchronize
+
+sftp_folders = [
+                'www/yu51a5.org/public_html/wp-content/themes/pinboard-child',
+                'www/yu51a5.org/backup',
+                'www/yu51a5.org/public_html/wp-content/uploads']
+
+for sf in sftp_folders:
+  synchronize(path_from=sf, path_to=os.path.join('website', os.path.basename(sf)), StorageFromType=StorageSFTP, StorageToType=StoragePCloud)
+
+# with cProfile.Profile() as pr:
+#with create_logging_object() as ll:
+  #folders_sftp_github = [['www/yu51a5.org/public_html/wp-content/themes/pinboard-child', 'pinboard-child'], ['www/yu51a5.org/backup', 'posts'], ['www/yu51a5.org/public_html/wp-content/themes', 'themes']]
+  #folders_pcloud_github = [['music/nein', 'b']]
+
+  #sync_contents(folders_sftp_github, StorageSFTP, StorageGitHub) #, StoragePCloud,)
+  #sync_contents([['music/warum', 'a']], StorageGitHub, StorageGitHub) 
+  # sync_contents([['tiny', 'e']], StorageGitHub, StoragePCloud) 
+  #sync_contents([['music/warum', 'e']], StorageGitHub, StoragePCloud)
