e71fcbb1ec5a808e0b3bf90b5de99e52cfb8894c
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2024-07-31 10:52:58 +0000

    added a warning to use `with` operator

--- StorageSFTP.py
+++ StorageSFTP.py
@@ -9,12 +9,14 @@ from settings import wp_images_extensions, default_ignore_wp_scaled_images
 #################################################################################
 class StorageSFTP(StorageBase):
 
-  def __init__(self, secret_name=None, ignore_wp_scaled_images=default_ignore_wp_scaled_images, logger_name=None, objects_to_sync_logger_with=[]):
-    super().__init__(constructor_kwargs=dict(secret_name=secret_name, ignore_wp_scaled_images=ignore_wp_scaled_images), logger_name=logger_name, objects_to_sync_logger_with=objects_to_sync_logger_with)
-    self.ignore_wp_scaled_images = ignore_wp_scaled_images
-
+  def __init__(self, secret_name=None, logger_name=None, objects_to_sync_logger_with=[]):
+    super().__init__(constructor_kwargs=dict(secret_name=secret_name), logger_name=logger_name, objects_to_sync_logger_with=objects_to_sync_logger_with)
+    
     secret_components = self._find_secret_components((4, 5), secret_name=secret_name)
     self.ssh_client_params = {what : secret_components[i] for i, what in enumerate(('hostname', 'port', 'username', 'password'))}
+
+    self.__ssh_client = None
+    self.__sftp_client = None
 
     if len(secret_components) == 5:
       # https://stackoverflow.com/questions/9963391/how-do-use-paramiko-rsakey-from-private-key
@@ -30,22 +32,31 @@ class StorageSFTP(StorageBase):
 
   ###############################################################################
   def _open(self):
-    self.ssh_client = paramiko.SSHClient()
+    self.__ssh_client = paramiko.SSHClient()
     # AutoAddPolicy explained in --> https://www.linode.com/docs/guides/use-paramiko-python-to-ssh-into-a-server/
-    self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    self.ssh_client.connect(**self.ssh_client_params)
-    self.sftp_client = self.ssh_client.open_sftp()
+    self.__ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    self.__ssh_client.connect(**self.ssh_client_params)
+    self.__sftp_client = self.__ssh_client.open_sftp()
     return self
 
   ###############################################################################
   def _close(self):
-    self.sftp_client.close()
-    self.ssh_client.close()
+    self.__sftp_client.close()
+    self.__ssh_client.close()
+    self.__ssh_client = None
+    self.__sftp_client = None
+
+  ###############################################################################
+  def __get_sftp_client(self):
+    if not self.__sftp_client:
+      self.log_critical("SFTP client not open, use `with StorageSFTP(<>) as s` instead of `s = StorageSFTP(<>)` ")
+    return self.__sftp_client
+    
 
   ###############################################################################
   def _get_filenames_and_directories(self, dir_name: str):
     
-    contents = self.sftp_client.listdir_attr(dir_name)
+    contents = self.__get_sftp_client().listdir_attr(dir_name)
 
     all_files, all_directories = [], []
     for entry in contents:
@@ -61,7 +72,7 @@ class StorageSFTP(StorageBase):
   ###############################################################################
   def _filter_out_files(self, files_):
     
-    if not self.ignore_wp_scaled_images:
+    if not default_ignore_wp_scaled_images:
       return files_
       
     qty_files = len(files_)
@@ -95,15 +106,15 @@ class StorageSFTP(StorageBase):
     
   ###############################################################################
   def _delete_file(self, filename):
-    self.sftp_client.remove(filename)
+    self.__get_sftp_client().remove(filename)
 
   ###############################################################################
   def _delete_directory(self, dirname):
-    self.sftp_client.rmdir(dirname)
+    self.__get_sftp_client().rmdir(dirname)
 
   ###############################################################################
   def _fetch_file_size_efficiently(self, filename):
-    result_raw = self.sftp_client.stat(filename)
+    result_raw = self.__get_sftp_client().stat(filename)
     result = result_raw.st_size #, 'modified' : result_raw.st_mtime
     return result
 
@@ -113,13 +124,13 @@ class StorageSFTP(StorageBase):
 
   ###############################################################################
   def _get_content(self, filename, length=None):
-    with self.sftp_client.open(filename) as sftp_file:
+    with self.__get_sftp_client().open(filename) as sftp_file:
       sftp_contents = sftp_file.read(size=length)
     return sftp_contents
 
   ###############################################################################
   def file_contents_is_text(self, filename):
-    with self.sftp_client.open(filename) as sftp_file:
+    with self.__get_sftp_client().open(filename) as sftp_file:
       sftp_contents = sftp_file.read(size=2048)
       result = StorageBase._file_contents_is_text(file_beginning=sftp_contents)
       return result
@@ -134,13 +145,13 @@ class StorageSFTP(StorageBase):
 
   ###############################################################################
   def _create_directory(self, dirname):
-    self.sftp_client.mkdir(dirname)
+    self.__get_sftp_client().mkdir(dirname)
 
   ###############################################################################
   def _rename_file(self, path_to_existing_file, path_to_new_file):
-    self.sftp_client.rename(path_to_existing_file, path_to_new_file)
+    self.__get_sftp_client().rename(path_to_existing_file, path_to_new_file)
     
   ###############################################################################
   def _rename_directory(self, path_to_existing_dir, path_to_new_dir):
-    self.sftp_client.rename(path_to_existing_dir, path_to_new_dir)
+    self.__get_sftp_client().rename(path_to_existing_dir, path_to_new_dir)
       
